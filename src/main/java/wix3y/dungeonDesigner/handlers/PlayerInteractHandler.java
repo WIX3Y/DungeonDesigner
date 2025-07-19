71c604152f59552075da099f8715b5acf83970d0
WIX3Y <wikstromdenice@gmail.com>
2025-07-19 14:05:43 +0200

    sound upon unlocking main gate pieces

--- src/main/java/wix3y/dungeonDesigner/handlers/PlayerInteractHandler.java
+++ src/main/java/wix3y/dungeonDesigner/handlers/PlayerInteractHandler.java
@@ -156,6 +156,7 @@ public class PlayerInteractHandler implements Listener {
                             }
                         }
                         player.sendMessage(MiniMessage.miniMessage().deserialize("<dark_gray>[<gradient:#00AA44:#99FFBB:#00AA44>Dungeons</gradient>]</dark_gray> <gray>>> You have unlocked " + numbOpenedKeys + "/" + numbKeys + " pieces of the main gate!"));
+                        player.playSound(player.getLocation(), Sound.BLOCK_BEACON_POWER_SELECT, 1, 1);
                         checkGateRemove(numbKeys, numbOpenedKeys, dungeonRewardInfo);
                     }
                     else {
