21d4cfc36e9ea53e55b5dfbae01ae65c64396774
WIX3Y <wikstromdenice@gmail.com>
2025-09-03 13:13:05 +0200

    Player dying in reward room bug fix

--- src/main/java/wix3y/dungeonDesigner/handlers/PlayerDeathHandler.java
+++ src/main/java/wix3y/dungeonDesigner/handlers/PlayerDeathHandler.java
@@ -42,7 +42,7 @@ public class PlayerDeathHandler implements Listener {
     public void onPlayerDeath(PlayerDeathEvent event) {
         Player player = event.getPlayer();
         RunningDungeonInfo runningDungeonInfo = playerDataUtil.getPlayerDungeonRunData(player.getUniqueId().toString());
-        if (runningDungeonInfo.getIsRunning()) {
+        if (runningDungeonInfo.getIsRunning() || runningDungeonInfo.getIsLooting()) {
             DungeonInfo dungeonInfo = configUtil.getDungeon(runningDungeonInfo.getDungeonName() + runningDungeonInfo.getDungeonInstance());
             plugin.forceExit(player, dungeonInfo);
             runningDungeonInfo.reset();
